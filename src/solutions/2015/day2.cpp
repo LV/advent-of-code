e0108f4026c7cf3fb013638ac0867084dac61d28
Luis Victoria <v@lambda.lv>
2024-12-28 18:24:17 -0500

    Solve 2015 day 2 part 2

--- src/solutions/2015/day2.cpp
+++ src/solutions/2015/day2.cpp
@@ -31,6 +31,14 @@ public:
     int smallest_side_area() {
         return std::min(width*length, std::min(length*height, width*height));
     }
+
+    int area() {
+        return length*width*height;
+    }
+
+    int least_ribbon() {
+        return std::min((length*2)+(width*2), std::min((width*2)+(height*2),(length*2)+(height*2)));
+    }
 };
 
 void solve_part1() {
@@ -46,7 +54,15 @@ void solve_part1() {
 }
 
 void solve_part2() {
-    std::string input = read_input(2015, 2);
+    std::vector<std::string> input = read_input_lines(2015, 2);
+
+    int total = 0;
+    for (std::string line : input) {
+        Rectangle r = Rectangle(line);
+        total += r.least_ribbon() + r.area();
+    }
+
+    std::cout << total << "\n";
 }
 
 } // END NAMESPACE
