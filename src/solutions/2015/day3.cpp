a57c3bf2b6b945a7af63dfae36df888118197307
Luis Victoria <v@lambda.lv>
2024-12-28 20:26:21 -0500

    Solve 2015 day 3 part 2

--- src/solutions/2015/day3.cpp
+++ src/solutions/2015/day3.cpp
@@ -35,7 +35,40 @@ void solve_part1() {
 }
 
 void solve_part2() {
-    return;
+    std::string input = read_input(2015, 3);
+
+    std::unordered_set<std::string> visited{};
+
+    int x_robo = 0;
+    int y_robo = 0;
+    int x_santa = 0;
+    int y_santa = 0;
+
+    bool isRoboMoving = true; // when false, Santa moves
+
+    for (char ch : input) {
+        if (isRoboMoving) {
+            if (ch == '^') y_robo++;
+            if (ch == 'v') y_robo--;
+            if (ch == '>') x_robo++;
+            if (ch == '<') x_robo--;
+            std::string coor = coordinate_to_string(x_robo, y_robo);
+            if (auto iter = visited.find(coor); iter == visited.end())
+                visited.insert(coor);
+            isRoboMoving = false;
+        } else {
+            if (ch == '^') y_santa++;
+            if (ch == 'v') y_santa--;
+            if (ch == '>') x_santa++;
+            if (ch == '<') x_santa--;
+            std::string coor = coordinate_to_string(x_santa, y_santa);
+            if (auto iter = visited.find(coor); iter == visited.end())
+                visited.insert(coor);
+            isRoboMoving = true;
+        }
+    }
+
+    std::cout << visited.size() << "\n";
 }
 
 } // END NAMESPACE
